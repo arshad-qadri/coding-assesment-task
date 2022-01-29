7a213cf6b6416b414524508ed5fe8164aaf2589a
arshad-qadri <arshadqadri321@gmail.com>
2022-01-29 16:26:14 +0000

    added loader

--- src/components/Pokmon.js
+++ src/components/Pokmon.js
@@ -5,6 +5,8 @@ const Pokmon = () => {
   const [data, setData] = useState(null);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+  const [mainLoader, setMainLoader] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   window.onscroll = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop ===
@@ -16,6 +18,7 @@ const Pokmon = () => {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(
         `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=${limit}`,
@@ -23,6 +26,8 @@ const Pokmon = () => {
       )
       .then((res) => {
         setData(res);
+        setIsLoading(false);
+        setMainLoader(false);
         console.log("res", res);
       })
       .catch((err) => {
@@ -33,36 +38,47 @@ const Pokmon = () => {
   return (
     <>
       <div className="container mx-auto pokmon-container">
-        {data?.data?.data.map((item, i) => {
-          return (
-            <div className="pokmon" key={i}>
-              <img src={item.images.small} alt={item.name} />
-              <div className="details">
-                <h5>{item.name}</h5> <div className="hp">hp : {item.hp} </div>
+        {!mainLoader ? (
+          data.data.data.map((item, i) => {
+            return (
+              <div className="pokmon" key={i}>
+                <img src={item.images.small} alt={item.name} />
+                <div className="details">
+                  <h5>{item.name}</h5> <div className="hp">hp : {item.hp} </div>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                    margin: "8px 0",
+                  }}
+                >
+                  <h5 className="m-0 ">Attacks :</h5>
+                  {item?.attacks?.map((x, ind) => (
+                    <span key={ind} style={{ marginLeft: "5px" }}>
+                      {x.name}
+                      {ind > -1 ? "," : ind - 1 ? "," : ""}
+                    </span>
+                  ))}
+                </div>
+                <p className="m-0">
+                  <b>Abilities</b> : N/A
+                </p>
               </div>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  flexWrap: "wrap",
-                  margin: "8px 0",
-                }}
-              >
-                <h5 className="m-0 ">Attacks :</h5>
-                {item?.attacks?.map((x, ind) => (
-                  <span key={ind} style={{ marginLeft: "5px" }}>
-                    {x.name}
-                    {ind > -1 ? "," : ind - 1 ? "," : ""}
-                  </span>
-                ))}
-              </div>
-              <p className="m-0">
-                <b>Abilities</b> : N/A
-              </p>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="loading">
+            <p>Loading...</p>
+          </div>
+        )}
       </div>
+      {!mainLoader && isLoading && (
+        <div className="loading">
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 };
